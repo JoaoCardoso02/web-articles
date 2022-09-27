b2d10308940a6153c66f76248d45c2f8ff48dae3
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-27 19:29:46 -0300

    test: add Card test

---
+++ src/components/Card/index.spec.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from ".";
+
+jest.mock('../../components/Author', () => {
+	return function AuthorComponent() {
+		return <div>Author Component Mock</div>
+	}
+})
+
+const props = {
+	title: "Card Title",
+	image: '/cardphoto.avif',
+	author: {
+		name: "Michael Scott",
+		date: "03 sep 2022",
+		image: "/photo.avif",
+	}
+};
+
+const renderComponent = () => {
+	return render(
+		<Card {...props} />
+	);
+} 
+
+describe("Card Component", () => {
+  test("should find title and image in document", () => {
+    const { getByText, getByAltText } = renderComponent()
+
+		const elementTitle = getByText(props.title)
+		const elementImage = getByAltText(`${props.title} image`)
+
+    expect(elementTitle).toBeInTheDocument();
+    expect(elementImage).toBeInTheDocument();
+  });
+
+	test("should find objectFit params equals cover for Image component", async () => {
+    const { getByAltText } = renderComponent()
+
+		const elementImage = getByAltText(`${props.title} image`)
+
+
+		expect(elementImage).toHaveStyle({ 'object-fit': 'cover' })
+  });
+
+	test("should have src property in Image component", async () => {
+    const { getByAltText } = renderComponent()
+
+		const elementImage = getByAltText(`${props.title} image`)
+
+
+		expect(elementImage).toHaveProperty('src')
+  });
+});
