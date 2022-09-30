e4cb3950a78fa0a3c9165f69e8dbaa2d034da876
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-30 19:27:38 -0300

    feat: add font weight in Chip text

--- src/components/Chip/styles.ts
+++ src/components/Chip/styles.ts
@@ -5,10 +5,12 @@ export const Btn = styled.button`
 	height: 24px;
 	background-color: #C8C8C8;
 	border-radius: 16px;
-	font-family: Inter;
-	font-size: 8px;
 	border: none;
 	outline: none;
 	padding: 14 7;
+
+	font-family: Inter;
+	font-size: 8px;
+	font-weight: 600;
 `;
 
