e6c97ca303fc26893984724b2a0f98bfed188056
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-10-01 22:12:10 -0300

    test: init Chip component test

---
+++ src/components/Chip/index.spec.tsx
@@ -0,0 +1,25 @@
+import React, { CSSProperties } from "react";
+import { render } from "@testing-library/react";
+import Chip from ".";
+
+const props = {
+	style: "color: red" as CSSProperties
+};
+
+const FakeChildren = () => <h1>my fake children</h1>
+
+const renderComponent = (children?: React.ReactNode) => {
+	return render(
+		<Chip {...props}>
+			{children}
+		</Chip>
+	);
+} 
+
+describe('Chip', () => {
+	it('should find its children successfully', () => {
+		const { baseElement } = renderComponent(FakeChildren)
+
+    expect(baseElement).toBeTruthy();
+	});
+})
