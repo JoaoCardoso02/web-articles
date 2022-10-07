75d4076afe55dc74c51d18a2f44114d95ea8b0d0
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-10-07 08:29:06 -0300

    feat: move Header to app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import Header from 'src/components/Header'
 import { GlobalStyle } from 'src/styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
 		<>
 			<GlobalStyle />
+			<Header />
 			<Component {...pageProps} />
 		</>
 	)
