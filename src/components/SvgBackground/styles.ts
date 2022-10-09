7d182f0e76b484f81c2101e058f87ae3e3a7706a
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-10-08 22:17:02 -0300

    fix: add z-index in background image container

--- src/components/SvgBackground/styles.ts
+++ src/components/SvgBackground/styles.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const ContainerDots = styled.div`
+	z-index: -9999;
 	position: fixed;
 	left: -10%;
 	bottom: -2%;
 `;
 
 export const ContainerEllipse = styled.div`
+	z-index: -9999;
 	position: fixed;
 	right: 1%;
 	bottom: -25%;
